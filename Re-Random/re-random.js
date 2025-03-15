2e9bce78e8e5f5e618ce0d486a84ca9038af6fc2
Hxttrick <andreasa.0404@gmail.com>
2025-03-15 13:57:50 +0100

    svg from github

--- Re-Random/re-random.js
+++ Re-Random/re-random.js
@@ -12,35 +12,6 @@
 (function() {
     'use strict';
 
-    function getSvg() {
-        const svgCode = `
-            <?xml version="1.0" encoding="utf-8"?>
-            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 96 96" style="enable-background:new 0 0 96 96;" xml:space="preserve">
-            <style type="text/css">
-                .st1{fill:none;stroke:#FFFFFF;stroke-width:6;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
-            </style>
-            <g id="Layer_1">
-                <g>
-                    <polygon class="st1" points="83,27.1 83,68.7 50.7,85.2 45.3,85.2 13,68.7 13,27.1 45,10.8 50.5,10.8"/>
-                    <g>
-                        <line class="st1" x1="23" y1="46.8" x2="23" y2="43.6"/>
-                        <line class="st1" x1="31" y1="58.4" x2="31" y2="55.2"/>
-                        <line class="st1" x1="38.9" y1="70.3" x2="38.9" y2="67.1"/>
-                        <line class="st1" x1="61" y1="66.4" x2="61" y2="63.2"/>
-                        <line class="st1" x1="71.1" y1="48.4" x2="71.1" y2="45.2"/>
-                        <line class="st1" x1="49.6" y1="27" x2="46.4" y2="27"/>
-                    </g>
-                    <g>
-                        <polyline class="st1" points="48,74.8 48,43.9 72.5,32.3"/>
-                        <line class="st1" x1="48" y1="43.9" x2="23.6" y2="32.3"/>
-                    </g>
-                </g>
-            </g>
-            </svg>
-        `;
-        return encodeURIComponent(svgCode.trim());
-    }
-
     function getMapId() {
         const nextDataEl = document.getElementById('__NEXT_DATA__');
         const nextData = JSON.parse(nextDataEl.textContent);
@@ -71,7 +42,7 @@
             inner.style.padding = "0";
 
             const img = document.createElement("img");
-            img.src = `data:image/svg+xml;charset=utf-8,${getSvg()}`;
+            img.src = "https://raw.githubusercontent.com/hxttrick/Geoguessr-Scripts/98f1feaa999c67871d6dca4d9b2fe17b6c3367fd/Re-Random/dice.svg";
             img.width = 24;
             img.height = 24;
 
